967c5181b97cd7f1852bec867710595eff5bfc04
Srihari V. Nanniyur <svn@localhost.localdomain>
2019-07-18 21:33:40 -0700

    update

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+@app.route("/mobile")
+def mobile():
+    return "Due to the dynamic nature of its website, Analytec does not currently support mobile viewing. Please use a Mac, PC, or laptop to view our site. Any suggestions and/or questions can be e-mailed to analytecofficial@gmail.com."
